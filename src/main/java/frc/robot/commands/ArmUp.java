2bca3bf56a5e44b70d36812abaeae010f85a9483
stacyc2449 <chens18@bxscience.edu>
2024-01-10 16:20:05 -0500

    imported to 2024

---
+++ src/main/java/frc/robot/commands/ArmUp.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ArmSim;
+
+public class ArmUp extends Command {
+  /** Creates a new ArmUp. */
+  private final ArmSim m_arm;
+  public ArmUp(ArmSim m_arm) {
+    this.m_arm = m_arm;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_arm);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    System.out.println("what");
+    System.out.println(m_arm.getAngle());
+    m_arm.armUp();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
